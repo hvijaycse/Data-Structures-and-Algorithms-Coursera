478fc1020bdc83b4a82838139920a8d9f4d9dbcc
Harsh Vijay <hvijay.cse@gmail.com>
2020-05-17 17:10:54 +0530

    week2

--- Algorithmic Toolbox/week2/7_fibonacci_partial_sum.py
+++ Algorithmic Toolbox/week2/7_fibonacci_partial_sum.py
@@ -1,13 +1,59 @@
+import random
+
 def fibonacci_partial_sum_naive(from_, to):
     sum = 0
     current = 0
     next  = 1
     for i in range(to + 1):
         if i >= from_:
-            sum = (sum + current )% 10
-        current, next = next % 10 ,(current + next )% 10
-    return sum 
+            sum = sum + current
+        current, next = next  ,current + next 
+    return sum % 10
+
+def fibonacci_sum_naive(n, m ):
+    previous = 0
+    current = 1
+    series = [0, 1]
+    for i in range( 100 - 1):
+        previous, current = current, current + previous
+        cur = current % 10 
+        if cur == 0:
+            previous, current = current, current + previous
+            cur2 = current % 10
+            if cur2 == 1:
+                break 
+            else:
+                series.append( cur)
+                series.append(cur2)
+        else:
+            series.append(cur)
+    length = len( series)
+    index = (n % length) + 2 
+    index = index % length
+    if not series[index]:
+        start =  9
+    else:
+        start =  series[index] -1 
+    
+    index = (m % length) + 2 
+    index = index % length
+    if not series[index]:
+        end =  9
+    else:
+        end =  series[index] -1
+    
+    if end - start < 0 :
+        return 9 - (end - start)
+    else:
+        return end - start
 
 if __name__ == '__main__':
-    from_, to = map(int, input().split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    while True:
+        from_, to = random.randint( 0, 100), random.randint(101, 1000)
+        ans1 = fibonacci_partial_sum_naive(from_, to)
+        ans2 = fibonacci_sum_naive( from_, to)
+        if ans1 == ans2:
+            print( "ok")
+        else:
+            print( from_, to, ans1, ans2)
+            break
--- Algorithmic Toolbox/week2/8_fibonacci_sum_squares.py
+++ Algorithmic Toolbox/week2/8_fibonacci_sum_squares.py
@@ -1,15 +1,32 @@
 # Uses python3
-def fibonacci_sum_squares_naive(n):
-    if n <= 1:
-        return n
+
+def get_fibonacci_huge_naive():
+    m = 10
     previous = 0
     current  = 1
-    sum1      = 1
-    for _ in range(n - 1):
+    sequense = [0, 1]
+    while True:
         previous, current = current, previous + current
-        previous, current = previous % 10, current % 10 
-        sum1 = ( sum1 + current* current) % 10
-    return sum1
+        s = current % m
+        if s == 0:
+            previous, current = current, previous + current
+            s2 = current % m
+            if s2 == 1:
+                break
+            else:
+                sequense.append( s)
+                sequense.append( s2)
+                continue
+        else:
+            sequense.append(s)
+    return sequense
+
+def fibonacci_sum_squares_naive(n):
+    series = get_fibonacci_huge_naive()
+    length = len(series)
+    a = series[ n % length]
+    b = series[ (n + 1) % length]
+    return (a*b) % 10
     
 if __name__ == '__main__':
     n = int(input())
